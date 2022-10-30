e320450d585b214a2c3cc689178f285a8f51a095
cnelf <1184974829@qq.com>
2022-10-31 00:33:25 +0800

    chore: support pwa

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import { VantResolver } from 'unplugin-vue-components/resolvers';
 import postcssPxToViewport from 'postcss-px-to-viewport';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import compressPlugin from 'vite-plugin-compression';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
@@ -55,6 +56,50 @@ export default ({ mode }) => {
             title: VITE_APP_TITLE
           }
         }
+      }),
+      VitePWA({
+        workbox: {
+          cacheId: 'custom-cache',
+          runtimeCaching: [
+            {
+              urlPattern: ({ url }) => url.origin === 'https://itunes.apple.com',
+              handler: 'NetworkFirst',
+              options: {
+                cacheName: 'api-cache',
+                cacheableResponse: {
+                  statuses: [200]
+                }
+              }
+            },
+            {
+              urlPattern: /(.*?)\.(png|jpe?g|svg|gif|bmp|psd|tiff|tga|eps)/,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'image-cache',
+                expiration: {
+                  // 最多30个图
+                  maxEntries: 30
+                }
+              }
+            },
+            {
+              urlPattern: /(.*?)\.(html|css|js)/,
+              handler: 'StaleWhileRevalidate',
+              options: {
+                cacheName: 'html-css-js-cache',
+                expiration: {
+                  maxEntries: 30, // 最多缓存30个，超过的按照LRU原则删除
+                  maxAgeSeconds: 30 * 24 * 60 * 60
+                },
+                cacheableResponse: {
+                  statuses: [200]
+                }
+              }
+            }
+          ]
+        },
+        // 取消注册
+        selfDestroying: false
       }),
       VITE_ENV === 'production' &&
         compressPlugin({
